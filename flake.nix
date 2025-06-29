5477d5b081eeb97e27541d06dc4fb9f8daab608d
Zie <53106860+skoove@users.noreply.github.com>
2025-06-29 16:37:34 +1000

    init flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development shell for undersea";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    fe_pkgs = fenix.packages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        fe_pkgs.stable.toolchain
+        pkg-config
+      ];
+    };
+  };
+}
